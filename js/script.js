ecf7afd0981799104530cdaaa811b86c5911c454
Colin Stell <colinjstell@gmail.com>
2019-03-08 12:49:05 -0700

    beginning of JS work, search input added, styling work

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+const page = document.querySelector('.page');
+const pageHeader = page.querySelector('.page-header');
+const studentList = page.querySelectorAll('li');
 
+//DYNAMICALLY ADDING SEARCH INPUT TO PAGE
+   //Creating The Elements
+   const searchDiv = document.createElement('div');
+   const searchInput = document.createElement('input');
+   const searchButton = document.createElement('button');
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
+   //Adding Classes, Attributes & Text Content to Search Elements
+   searchDiv.className = "student-search";
+   searchInput.type = 'text';
+   searchInput.placeholder = "Search for students...";
+   searchButton.textContent = "Search";
+   //Appending Elements to The Page
+   searchDiv.appendChild(searchInput);
+   searchDiv.appendChild(searchButton);
+   pageHeader.appendChild(searchDiv);
 
 
 
