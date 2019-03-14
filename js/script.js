d180e80d06f447f0d34ffd9ab0a9dcfc25bc7804
Colin Stell <colinjstell@gmail.com>
2019-03-14 08:25:20 -0700

    search funcitonality work

--- js/script.js
+++ js/script.js
@@ -1,78 +1,103 @@
 //SELECTING MAIN ELEMENTS
-   const page = document.querySelector('.page');
-   const pageHeader = page.querySelector('.page-header');
+const page = document.querySelector('.page');
+const pageHeader = page.querySelector('.page-header');
 
 //DYNAMICALLY ADDING SEARCH INPUT TO PAGE
-   //Creating The Elements
-   const searchDiv = document.createElement('div');
-   const searchInput = document.createElement('input');
-   const searchButton = document.createElement('button');
+//Creating The Elements
+const searchDiv = document.createElement('div');
+const searchInput = document.createElement('input');
+const searchButton = document.createElement('button');
 
-   //Adding Classes, Attributes & Text Content to Search Elements
-   searchDiv.className = "student-search";
-   searchInput.type = 'text';
-   searchInput.placeholder = "Search for students...";
-   searchButton.textContent = "Search";
-   //Appending Elements to The Page
-   searchDiv.appendChild(searchInput);
-   searchDiv.appendChild(searchButton);
-   pageHeader.appendChild(searchDiv);
+//Adding Classes, Attributes & Text Content to Search Elements
+searchDiv.className = "student-search";
+searchInput.type = 'text';
+searchInput.placeholder = "Search for students...";
+searchButton.textContent = "Search";
+//Appending Elements to The Page
+searchDiv.appendChild(searchInput);
+searchDiv.appendChild(searchButton);
+pageHeader.appendChild(searchDiv);
 
 //PAGE & STUDENT LIST VALUES
-   const maxStudents = 10;
-   const studentList = page.querySelectorAll('li');
-   const studentListLen = studentList.length;
-   let pageNumber = 1;
+const maxStudents = 10;
+let studentList = page.querySelectorAll('li');
+const studentListLen = studentList.length;
+let pageNumber = 1;
 
 // SHOW PAGE FUNCTION
 
-   showPage = (list, page) => {
-      let start = ((page - 1) * maxStudents);
-      console.log('StartIndex:' + start);
-      let end = ((start + maxStudents) - 1);
-      console.log('End Index:' + end);
-      for (let i = 0; i < list.length; i += 1) {
-         if (i >= start && i <= end) {
-            list[i].style.display = 'block';
-         } else {
-            list[i].style.display = 'none';
-         }
+showPage = (list, page) => {
+   let start = ((page - 1) * maxStudents);
+   let end = ((start + maxStudents) - 1);
+   for (let i = 0; i < list.length; i += 1) {
+      list[i].style.display = 'none';
+      if (i >= start && i <= end) {
+         list[i].style.display = 'block';
+      } else {
+         list[i].style.display = 'none';
       }
-   };
+   }
+};
 
 // APPEND PAGE FUNCTION
 
-   appendPageLinks = (list) => {
-      const pagesNum = Math.ceil(list.length / maxStudents);
-      const pagination = document.createElement('div');
-      const pagUl = document.createElement('ul');
+appendPageLinks = (list) => {
+   const pagesNum = Math.ceil(list.length / maxStudents);
+   const pagination = document.createElement('div');
+   const pagUl = document.createElement('ul');
 
-      pagination.className = 'pagination';
-      page.appendChild(pagination);
-      pagination.appendChild(pagUl);
+   pagination.className = 'pagination';
+   page.appendChild(pagination);
+   pagination.appendChild(pagUl);
 
-      for (let i = 1; i < pagesNum + 1; i += 1){
-         let liContent = `
+   for (let i = 1; i < pagesNum + 1; i += 1) {
+      let liContent = `
             <li>
                <a href="#">${i}</a>
             </li>
             `;
-         pagUl.innerHTML += liContent;
-      }
+      pagUl.innerHTML += liContent;
+   }
 
-      const pageLinks = document.querySelectorAll('a');
-      pageLinks[0].className = "active";
+   const pageLinks = document.querySelectorAll('a');
+   pageLinks[0].className = "active";
 
-      for(let i = 0; i < pageLinks.length; i ++) {
-         pageLinks[i].addEventListener('click', function (event) {
-            let active = document.querySelector('.active');
-            if (active) {
-               active.classList.remove('active');
-            }
-            showPage(studentList, pageNumber += 1);
-            event.target.className = "active";
-         });
+   for (let i = 0; i < pageLinks.length; i++) {
+      pageLinks[i].addEventListener('click', function (event) {
+         let active = document.querySelector('.active');
+         if (active) {
+            active.classList.remove('active');
+         }
+         showPage(studentList, i + 1);
+         event.target.className = "active";
+      });
+   }
+};
+
+showPage(studentList, pageNumber);
+appendPageLinks(studentList);
+
+// SEARCH FUNCTION
+const input = document.querySelector('input');
+const submit = document.querySelector('button');
+
+filter = (list) => {
+   inputValue = input.value.toUpperCase();
+   for (let i = 0; i < list; i++) {
+      studentName = list[i].getElementsByTagName('h3')[0];
+      studentValue = studentName.value;
+      if (studentValue.toUpperCase().indexOf(inputValue) == -1) {
+         return list[i].style.display = '';
+      } else {
+         return list[i].style.display = 'none';
       }
-   };
-   showPage(studentList, pageNumber);
-   appendPageLinks(studentList);
+   }
+}
+
+input.addEventListener('keyup', () => {
+   filter(studentList);
+})
+submit.addEventListener('submit', (event) => {
+   event.preventDefault();
+   filter(studentList);
+})
